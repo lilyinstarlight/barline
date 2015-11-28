8bb097d895bc7671caa3dbfe4083778776e0df46
Foster McLane <fkmclane@gmail.com>
2015-11-28 10:50:23 -0500

    remove useless ifdef's for WLAN

--- src/data.h
+++ src/data.h
@@ -8,9 +8,7 @@ int getmem();
 #ifdef ALSA
 int getvol(const char * card, const char * selement);
 #endif
-#ifdef WLAN
 int getwlan(const char * iface, char * ssid, size_t size);
-#endif
 int getbatt(const char * batt);
 int gettime(char * buf, size_t size, const char * fmt, const char * tz);
 #endif
