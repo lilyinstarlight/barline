5817dc7217a50e13ed6bb38f4d83970ac4ab6916
Foster McLane <fkmclane@gmail.com>
2015-12-10 10:34:09 -0500

    finish format defaults and fix printing by keeping two indices

--- src/format.c
+++ src/format.c
@@ -6,6 +6,10 @@
 
 #include "format.h"
 
+const int core_id = -1;
+
+const char * temp_thermal = "thermal_zone0";
+
 const char * mem_units[] = { "KiB", "MiB", "GiB" };
 
 const char * vol_card = "default";
@@ -15,11 +19,14 @@ const char * vol_mutestr = "mute";
 const char * wlan_iface = "wlan0";
 const char * wlan_offstr = "off";
 
+const char * batt_power = "BAT0";
+
 const char * time_tz = "UTC";
 const char * time_fmt = "%R";
 
 void format(char * buf, size_t size, const char * format) {
-	size_t idx;
+	size_t fidx;
+	size_t bidx;
 	size_t last;
 	size_t len;
 	int ret;
@@ -30,10 +37,11 @@ void format(char * buf, size_t size, const char * format) {
 	char * arg;
 
 	//core
-	int coreid;
+	int coreid = core_id;
 	int core;
 
 	//temp
+	const char * thermal = temp_thermal;
 	int temp;
 
 	//mem
@@ -49,53 +57,62 @@ void format(char * buf, size_t size, const char * format) {
 	int off;
 
 	//batt
+	const char * power = batt_power;
 	int batt;
 
 	//time
 	const char * tz = time_tz, * fmt = time_fmt;
 	char time[32];
-	int good;
+	int bad;
 
 	len = strlen(format);
 
-	for(idx = 0; idx < len; idx++) {
-		if(idx >= size - 1)
-			break;
+	for(fidx = 0, bidx = 0; fidx < len && bidx < size - 1; fidx++) {
+		if(format[fidx] == '%' && fidx + 1 < len && format[fidx + 1] == '{') {
+			ret = sscanf(format + fidx, "%%{%c%63[^}]}", &cmd, args);
 
-		if(format[idx] == '%' && idx + 1 < len && format[idx + 1] == '{') {
-			ret = sscanf(format + idx, "%%{%c%63[^}]}", &cmd, args);
-
-			end = strchr(format + idx, '}');
+			end = strchr(format + fidx, '}');
 			if(end == NULL)
 				last = len - 1;
 			else
 				last = end - format;
 
-			if(ret != 2) {
+			if(ret == 1) {
+				args[0] = '\0';
+			}
+			else if(ret != 2) {
+				buf[bidx] = format[fidx];
+				bidx++;
+
 				fprintf(stderr, "warning: invalid syntax in format string ignored");
 				continue;
 			}
 
 			switch(cmd) {
 				case 'C':
-					ret = sscanf(args, "%d", &coreid);
-					if(ret != 1) {
-						fprintf(stderr, "warning: invalid core id (%s) ignored", args);
-						break;
+					if(*args != '\0') {
+						ret = sscanf(args, "%d", &coreid);
+						if(ret != 1) {
+							fprintf(stderr, "warning: invalid core id (%s) ignored", args);
+							break;
+						}
 					}
 
 					core = getcore(coreid);
-					snprintf(buf + idx, size - 1 - idx, "%d %%", core);
+					bidx += snprintf(buf + bidx, size - 1 - bidx, "%d %%", core);
 
 					break;
 				case 'H':
-					temp = gettemp(args);
-					snprintf(buf + idx, size - 1 - idx, "%d °C", temp);
+					if(*args != '\0')
+						thermal = args;
+
+					temp = gettemp(thermal);
+					bidx += snprintf(buf + bidx, size - 1 - bidx, "%d °C", temp);
 
 					break;
 				case 'M':
 					mem = getmem();
-					format_unit(buf + idx, size - 1 - idx, mem, mem_units, sizeof(mem_units));
+					bidx += format_unit(buf + bidx, size - 1 - bidx, mem, mem_units, sizeof(mem_units));
 
 					break;
 				case 'V':
@@ -103,21 +120,21 @@ void format(char * buf, size_t size, const char * format) {
 					if(*arg != '\0') {
 						card = arg;
 
-						arg = strchr(arg, ':') + 1;
+						arg = strchr(arg, ':');
 						if(arg != NULL) {
-							selement = arg;
+							selement = arg + 1;
 
-							arg = strchr(arg, ':') + 1;
+							arg = strchr(arg + 1, ':');
 							if(arg != NULL)
-								mutestr = arg;
+								mutestr = arg + 1;
 						}
 					}
 
 					vol = getvol(card, selement);
 					if(vol / 1000)
-						snprintf(buf + idx, size - 1 - idx, "%s", mutestr);
+						bidx += snprintf(buf + bidx, size - 1 - bidx, "%s", mutestr);
 					else
-						snprintf(buf + idx, size - 1 - idx, "%d %%", vol % 101);
+						bidx += snprintf(buf + bidx, size - 1 - bidx, "%d %%", vol % 101);
 
 					break;
 				case 'W':
@@ -125,24 +142,27 @@ void format(char * buf, size_t size, const char * format) {
 					if(*arg != '\0') {
 						iface = arg;
 
-						arg = strchr(arg, ':') + 1;
+						arg = strchr(arg, ':');
 						if(arg != NULL)
-							offstr = arg;
+							offstr = arg + 1;
 					}
 
 					off = getwlan(iface, ssid, sizeof(ssid));
 					if(off)
-						snprintf(buf + idx, size - 1 - idx, "%s", offstr);
+						bidx += snprintf(buf + bidx, size - 1 - bidx, "%s", offstr);
 					else
-						snprintf(buf + idx, size - 1 - idx, "%s", ssid);
+						bidx += snprintf(buf + bidx, size - 1 - bidx, "%s", ssid);
 
 					break;
 				case 'E':
-					batt = getbatt(args);
+					if(*args != '\0')
+						power = args;
+
+					batt = getbatt(power);
 					if(batt / 1000)
-						snprintf(buf + idx, size - 1 - idx, "^%d %%", batt % 101);
+						bidx += snprintf(buf + bidx, size - 1 - bidx, "^%d %%", batt % 101);
 					else
-						snprintf(buf + idx, size - 1 - idx, "%d %%", batt % 101);
+						bidx += snprintf(buf + bidx, size - 1 - bidx, "%d %%", batt % 101);
 
 					break;
 				case 'D':
@@ -150,29 +170,31 @@ void format(char * buf, size_t size, const char * format) {
 					if(*arg != '\0') {
 						tz = arg;
 
-						arg = strchr(arg, ':') + 1;
+						arg = strchr(arg, ':');
 						if(arg != NULL)
-							fmt = arg;
+							fmt = arg + 1;
 					}
 
-					good = gettime(time, sizeof(time), fmt, tz);
-					if(good)
-						snprintf(buf + idx, size - 1 - idx, "%s", time);
+					bad = gettime(time, sizeof(time), fmt, tz);
+					if(bad)
+						bidx += snprintf(buf + bidx, size - 1 - bidx, "%s", "none");
 					else
-						snprintf(buf + idx, size - 1 - idx, "%s", "none");
+						bidx += snprintf(buf + bidx, size - 1 - bidx, "%s", time);
 
 					break;
 				default:
-					memcpy(buf + idx, format + idx, last - idx);
+					memcpy(buf + bidx, format + fidx, last - fidx);
+					bidx += last - fidx;
 					break;
 			}
 
-			idx = last;
+			fidx = last;
 		}
 		else {
-			buf[idx] = format[idx];
+			buf[bidx] = format[fidx];
+			bidx++;
 		}
 	}
 
-	buf[size - 1] = '\0';
+	buf[bidx] = '\0';
 }
