6238424a1be6a555551b138c1851912bbd8cdeab
Foster McLane <fkmclane@gmail.com>
2016-06-23 08:03:43 -0400

    add widget structure

--- src/widget.h
+++ src/widget.h
@@ -1,3 +1,33 @@
 #ifndef WIDGET_H
 #define WIDGET_H
+typedef struct widget {
+	struct widget * prev;
+
+	enum {
+		BATT,
+		CPU,
+		MEM,
+		TEMP,
+		TIME,
+		VOL,
+		WIN,
+		WLAN,
+		WORK,
+	} type;
+
+	union {
+		batt_t * batt;
+		cpu_t * cpu;
+		mem_t * mem;
+		temp_t * temp;
+		time_t * time;
+		vol_t * vol;
+		win_t * win;
+		wlan_t * wlan;
+		work_t * work;
+	} data;
+} widget_t;
+
+void widget_parse(const char * widget, widget_t * widget);
+size_t widget_format(const widget_t * widget, char * buf, size_t size);
 #endif
