0de1f4d45a672137fb0cb87a2e00d5d7b280d6bd
Foster McLane <fkmclane@gmail.com>
2016-12-12 13:54:48 -0500

    remove debugging symbols

--- config.mk
+++ config.mk
@@ -3,8 +3,7 @@ BIN=barline
 SRC=barline.c config.c format.c bspwm.c widget.c batt.c cpu.c mem.c temp.c text.c timedate.c vol.c win.c wlan.c work.c
 
 CC?=cc
-#CFLAGS+=-Wall -Wextra -Wpedantic -Werror -std=c11 -D_DEFAULT_SOURCE
-CFLAGS+=-Wall -Wextra -Wpedantic -Werror -Wno-unused-parameter -Wno-format-zero-length -std=c11 -D_DEFAULT_SOURCE -ggdb
+CFLAGS+=-Wall -Wextra -Wpedantic -Werror -Wno-unused-parameter -Wno-format-zero-length -std=c11 -D_DEFAULT_SOURCE
 LDLIBS=-lasound -lxcb
 
 SRCDIR=src
